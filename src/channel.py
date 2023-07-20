a34f381421b64008d600c4c7cf8e29a71fe7b62a
Данила <spook9400@mail.ru>
2023-07-20 07:53:01 +0300

    ddacs

--- src/channel.py
+++ src/channel.py
@@ -4,23 +4,43 @@ import os
 # необходимо установить через: pip install google-api-python-client
 from googleapiclient.discovery import build
 
-
 # необходимо установить через: pip install google-api-python-client
 from googleapiclient.discovery import build
+
 
 class Channel:
     """Класс для ютуб-канала"""
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
+        channel = self.get_service().channels().list(id=channel_id, part='snippet,statistics').execute()["items"][0]
+        print(channel)
         self.__channel_id = channel_id
+        self.title = channel["snippet"]["title"]
+        self.url = "https://www.youtube.cpm/channel/" + channel_id
+        self.subscriberCount = int(channel["statistics"]["subscriberCount"])
+        self.video_count = channel["statistics"]["viewCount"]
 
-        self.title = None
-        self.description = None
-        self.url = None
-        self.subscriberCount = None
-        self.video_count = None
-        self.viewCount = None
+    def __str__(self):
+        return f'{self.title} ({self.url})'
+
+    def __add__(self, other):
+        return self.subscriberCount + other.subscriberCount
+
+    def __sub__(self, other):
+        return self.subscriberCount - other.subscriberCount
+
+    def __eq__(self, other):
+        return self.subscriberCount == other.subscriberCount
+
+    def __lt__(self, other):
+        return self.subscriberCount < other.subscriberCount
+
+    def __le__(self, other):
+        return self.subscriberCount <= other.subscriberCount
+
+    def __gt__(self, other):
+        return self.subscriberCount > other.subscriberCount
 
     @property
     def channel_id(self):
@@ -30,8 +50,6 @@ class Channel:
     def get_service(cls):
         api_key: str = os.getenv('YT_API_KEY')
         return build('youtube', 'v3', developerKey=api_key)
-
-
 
     # def print_info(self) -> None:
     #     Получает данные по API ключу
@@ -65,12 +83,9 @@ class Channel:
         with open(file_name, 'w') as file:
             json.dump(data, file, indent=2, ensure_ascii=False)
 
-
-
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         api_key: str = os.getenv('YT_API_KEY')
         youtube = build('youtube', 'v3', developerKey=api_key)
         dict_to_print = youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
         print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
-
---
+++ src/video.py
@@ -0,0 +1,24 @@
+import json
+from src.channel import Channel
+
+
+class Video:
+    def __init__(self, video_id):
+        self.video_id = video_id
+        youtube = Channel.get_service()
+        video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                               id=self.video_id
+                                               ).execute()
+        self.title = video_response["items"][0]["snippet"]["title"]
+        self.url = "https://www.youtube.com/watch?v=" + self.video_id
+        self.views_count = video_response["items"][0]["statistics"]["viewCount"]
+        self.likes_count = video_response["items"][0]["statistics"]["viewCount"]
+
+    def __str__(self):
+        return self.title
+
+
+class PLVideo(Video):
+    def __init__(self, video_id, pl_video):
+        super().__init__(video_id)
+        self.pl_video = pl_video

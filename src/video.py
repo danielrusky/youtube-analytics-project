d87f6232d74f8bfb7b69016edddc09f9c6863170
Данила <spook9400@mail.ru>
2023-07-27 17:32:44 +0300

    homework-6 youtube_analytics

--- src/video.py
+++ src/video.py
@@ -4,15 +4,23 @@ from src.channel import Channel
 
 class Video:
     def __init__(self, video_id):
+
         self.video_id = video_id
         youtube = Channel.get_service()
-        video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                               id=self.video_id
-                                               ).execute()
-        self.title = video_response["items"][0]["snippet"]["title"]
-        self.url = "https://www.youtube.com/watch?v=" + self.video_id
-        self.views_count = video_response["items"][0]["statistics"]["viewCount"]
-        self.likes_count = video_response["items"][0]["statistics"]["viewCount"]
+        try:
+            video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                   id=video_id
+                                                   ).execute()
+            self.title = video_response["items"][0]["snippet"]["title"]
+            self.url = "https://www.youtube.com/watch?v=" + self.video_id
+            self.views_count = video_response["items"][0]["statistics"]["viewCount"]
+            self.like_count = video_response["items"][0]["statistics"]["viewCount"]
+        except IndexError:
+            self.video_id = video_id
+            self.title = None
+            self.url = None
+            self.views_count = None
+            self.like_count = None
 
     def __str__(self):
         return self.title
